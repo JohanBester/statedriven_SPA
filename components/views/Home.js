d1f748ae18dffeaa42298bdab14b747cc1830cb4
Johan Bester <johan.bester@yahoo.com>
2022-03-02 21:48:14 -0600

    updates

--- components/views/Home.js
+++ components/views/Home.js
@@ -2,8 +2,6 @@ import html from "html-literal";
 export default () => html`
   <section id="jumbotron">
     <h2>Savvy Coders Jan. 2020 Cohort</h2>
-    <a href="" onClick="alert('Hello! You clicked the Button!')"
-      >"Call to Action" "Button"</a
-    >
+    <a href="" onClick="alert('Hello! You clicked the Button!')">"Call to Action" "Button"</a>
   </section>
 `;
